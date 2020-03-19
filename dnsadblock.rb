615c39881178a1abf279db488d99df63280f6a92
dnsadblock <office@dnsadblock.com>
2020-03-19 20:56:29 +0200

    Brew formula update for dnsadblock version v0.2.3

--- dnsadblock.rb
+++ dnsadblock.rb
@@ -2,24 +2,24 @@
 class Dnsadblock < Formula
   desc "dnsadblock DNS Proxy"
   homepage "https://dnsadblock.com"
-  version "0.1.0"
+  version "0.2.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.1.0/dnsadblock_0.1.0_darwin_amd64.tar.gz"
-    sha256 "df719f931887cc29536f98a322d6bce71bb95a370e7379fd0345e41c6b97c12c"
+    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.3/dnsadblock_0.2.3_darwin_amd64.tar.gz"
+    sha256 "3b6f19e0e1cb042683a7a2b1703799d3c349793eeffd4697176083cdec5a7220"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.1.0/dnsadblock_0.1.0_linux_amd64.tar.gz"
-      sha256 "2736b65bc10ddb3cd9acc603bf97b51772fadab961ccae66706624f2c595dc39"
+      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.3/dnsadblock_0.2.3_linux_amd64.tar.gz"
+      sha256 "f4846da4d1360cca42fbd38891d9bdf5738d49b9c5c6e4a0320509c8d298b55a"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.1.0/dnsadblock_0.1.0_linux_arm64.tar.gz"
-        sha256 "245ee87cf8bf054a264d5e461a7fed552d8c055c0bdb4bceaaf49b8a27c3e752"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.3/dnsadblock_0.2.3_linux_arm64.tar.gz"
+        sha256 "834e49a582fb22b987e671bdb754103926ccb55d094e318baf58b3eb828ceb47"
       else
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.1.0/dnsadblock_0.1.0_linux_armv6.tar.gz"
-        sha256 "f0623d138ddca8d5feffdec83a374edeb04681531f0abd58426cc56de3015cbe"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.3/dnsadblock_0.2.3_linux_armv6.tar.gz"
+        sha256 "d63bf12112c505cadafb2fdc049cdcb6c89ad77db5bb083fa597ca23e1dfc922"
       end
     end
   end
