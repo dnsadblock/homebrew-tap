c98bd5ff500fa306bf91faaba684bd49b2bd8f0b
dnsadblock <office@dnsadblock.com>
2020-03-21 23:07:05 +0200

    Brew formula update for proxy version v0.2.33-beta.19

--- proxy.rb
+++ proxy.rb
@@ -2,24 +2,24 @@
 class Proxy < Formula
   desc "dnsadblock DNS Proxy"
   homepage "https://dnsadblock.com"
-  version "0.2.33-beta.18"
+  version "0.2.33-beta.19"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.18/proxy_0.2.33-beta.18_darwin_amd64.tar.gz"
-    sha256 "9da075c8620bcf13903bd751b63b09bdf2de5cb076a11b4fdcf5cff343166811"
+    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.19/proxy_0.2.33-beta.19_darwin_amd64.tar.gz"
+    sha256 "21ba7d8dbf8e0d4d3883bca787fd2369068ab16f3332ff68017c08ed27832842"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.18/proxy_0.2.33-beta.18_linux_amd64.tar.gz"
-      sha256 "61ed73a805708fba5868f2f83315a072bca920ce41f693479644097dae205050"
+      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.19/proxy_0.2.33-beta.19_linux_amd64.tar.gz"
+      sha256 "b1cf3d816c91cfea8bc89f450e33495a7d8f2b96359d756ce87ca40c77a3f878"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.18/proxy_0.2.33-beta.18_linux_arm64.tar.gz"
-        sha256 "6617e6f00391186956e6e2dfe58a737e41b81f5005a65a5eb5b32b6dcff003dc"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.19/proxy_0.2.33-beta.19_linux_arm64.tar.gz"
+        sha256 "47e261e0f072ead53a08e86467382c26b692e5bec325334aefb60201fc4102ca"
       else
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.18/proxy_0.2.33-beta.18_linux_armv6.tar.gz"
-        sha256 "48f6cb4fab5aac65316cbad7be2249ed7febff10852ae3846c13091c5e103c55"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.33-beta.19/proxy_0.2.33-beta.19_linux_armv6.tar.gz"
+        sha256 "7cd159d82df4b4d2c9a45ef501651d95d28064392550993990c10f8028bb6df3"
       end
     end
   end
