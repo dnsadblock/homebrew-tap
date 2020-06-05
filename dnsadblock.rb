ad7337afd29be7506be1877e0e26f125da35978f
dnsadblock <office@dnsadblock.com>
2020-06-05 07:08:47 +0300

    Brew formula update for dnsadblock version v0.2.58

--- dnsadblock.rb
+++ dnsadblock.rb
@@ -2,24 +2,24 @@
 class Dnsadblock < Formula
   desc "dnsadblock DNS Proxy"
   homepage "https://dnsadblock.com"
-  version "0.2.57"
+  version "0.2.58"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.57/dnsadblock_0.2.57_darwin_amd64.tar.gz"
-    sha256 "e4eab25d434ffba405249142208d2d2049c8ed0370bc9d30c2f9204c455f1866"
+    url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.58/dnsadblock_0.2.58_darwin_amd64.tar.gz"
+    sha256 "52da3d807fff27969e515d4324daed410c88e0c10ed764b2e5f8a1e3ce55a85e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.57/dnsadblock_0.2.57_linux_amd64.tar.gz"
-      sha256 "6da29dd74dc8606b62950edc5a99d66e1c32ff5ae4d8a8465a65e6ed4af1747b"
+      url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.58/dnsadblock_0.2.58_linux_amd64.tar.gz"
+      sha256 "482e011932b8a7d64c41aaf7da456d0c82cb2c0b135ef741255fcf568b1783f3"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.57/dnsadblock_0.2.57_linux_arm64.tar.gz"
-        sha256 "e12348262b012119f491f56eeb7058f62afae3d4f3046879345c5a28a2e666b7"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.58/dnsadblock_0.2.58_linux_arm64.tar.gz"
+        sha256 "97106154728dfbcc8917e7424086361536bee0e53f137727d7e529fff13277f0"
       else
-        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.57/dnsadblock_0.2.57_linux_armv6.tar.gz"
-        sha256 "7a4a528c9af611f260c3a033e888bf299de91bd35e90f098d802a4c39922e9cb"
+        url "https://github.com/dnsadblock/proxy-release/releases/download/v0.2.58/dnsadblock_0.2.58_linux_armv6.tar.gz"
+        sha256 "ae43a1bb89e1cf1f7b1f356b87c0dececf2f389aaf190e5de90b64f4ee41323f"
       end
     end
   end
